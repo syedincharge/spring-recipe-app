f5cf0d3c9b67144f871d782d9da04b9e1933fd48
syedincharge <rizvisyedba@gmail.com>
2022-03-18 00:49:37 -0600

    Create Enum Class and Relate to Recipe Entity.

---
+++ src/main/java/com/rizvi/spring/domain/Difficulty.java
@@ -0,0 +1,6 @@
+package com.rizvi.spring.domain;
+
+public enum Difficulty {
+
+    EASY, MODERATE, HARD
+}
--- src/main/java/com/rizvi/spring/domain/Recipe.java
+++ src/main/java/com/rizvi/spring/domain/Recipe.java
@@ -16,14 +16,15 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    // todo
-    //private Difficulty difficulty;
+
     @OneToMany(cascade =CascadeType.ALL, mappedBy="recipe")
     private Set<Ingredient> ingredients;
 
 
     @Lob
     private Byte[] image;
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToOne(cascade= CascadeType.ALL)
     private Notes notes;
@@ -114,5 +115,13 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
