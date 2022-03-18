3ab0738d0e0690f3996af682376f1a84b293f2e9
syedincharge <rizvisyedba@gmail.com>
2022-03-18 00:41:46 -0600

    Update Recipe and Ingredient Entity classes.

--- src/main/java/com/rizvi/spring/domain/Ingredient.java
+++ src/main/java/com/rizvi/spring/domain/Ingredient.java
@@ -11,7 +11,8 @@ public class Ingredient {
     private Long Id;
     private String description;
     private BigDecimal amount;
-    //private unitOfMeasure uom;
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
     @ManyToOne
     private Recipe recipe;
 
@@ -45,5 +46,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 }
