0cc3199a403bd751ac5fe71985f614450035a18e
syedincharge <rizvisyedba@gmail.com>
2022-03-18 01:12:54 -0600

    Setting relationship recipeId and categoryId.

--- src/main/java/com/rizvi/spring/domain/Recipe.java
+++ src/main/java/com/rizvi/spring/domain/Recipe.java
@@ -20,7 +20,6 @@ public class Recipe {
     @OneToMany(cascade =CascadeType.ALL, mappedBy="recipe")
     private Set<Ingredient> ingredients;
 
-
     @Lob
     private Byte[] image;
     @Enumerated(value = EnumType.STRING)
@@ -28,6 +27,11 @@ public class Recipe {
 
     @OneToOne(cascade= CascadeType.ALL)
     private Notes notes;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name="recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -123,5 +127,13 @@ public class Recipe {
 
     public void setDifficulty(Difficulty difficulty) {
         this.difficulty = difficulty;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
